badd93bee29e44eb7d441ff3b5fe697f3defe678
Khushboo Chauhan <166603855+khushboo0824@users.noreply.github.com>
2024-10-14 16:37:18 +0530

    Add files via upload

---
+++ scripts/preprocess.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from datasets import load_dataset
+from transformers import DistilBertTokenizer
+
+
+
+# Load the IMDB dataset
+imdb_dataset = load_dataset('imdb')
+
+# Extract the train and test splits
+train_data = imdb_dataset['train']
+test_data = imdb_dataset['test']
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+
+def tokenize_function(examples):
+    return tokenizer(examples['text'], truncation=True, padding='max_length', max_length=128)
+
+# Tokenize the train and test datasets
+tokenized_train = train_data.map(tokenize_function, batched=True)
+tokenized_test = test_data.map(tokenize_function, batched=True)
+
+tokenized_train.save_to_disk('D:/NLP/data/processed')
+tokenized_test.save_to_disk('D:/NLP/data/processed')
